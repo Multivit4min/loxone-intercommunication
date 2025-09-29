bfb1b04a29b79e6d1274816071f3b2154400d61c
David Kartnaller <david.kartnaller@gmail.com>
2025-09-29 23:49:56 +0000

    set properties of tunablewhite

--- src/packet/payload/SmartActuatorTunableWhitePayload.ts
+++ src/packet/payload/SmartActuatorTunableWhitePayload.ts
@@ -1,11 +1,26 @@
 import { Payload } from "./Payload"
 
-//@todo
 export class SmartActuatorTunableWhitePayload extends Payload {
+
+  /** temperature in kelvin */
+  get temperature() {
+    return this.buffer.readUInt16LE(0)
+  }
+
+  /** brightness in % */
+  get brightness() {
+    return this.buffer.readUint8(2)
+  }
+
+  get fadeTime() {
+    return this.buffer.readUint16LE(4) / 10
+  }
 
   get value(): SmartActuatorTunableWhitePayload.Type {
     return {
-      buffer: this.buffer
+      temperature: this.temperature,
+      brightness: this.brightness,
+      fadeTime: this.fadeTime
     }
   }
 
@@ -19,7 +34,12 @@ export class SmartActuatorTunableWhitePayload extends Payload {
 export namespace SmartActuatorTunableWhitePayload {
 
   export type Type = {
-    buffer: Buffer
+    /** temperature in kelvin */
+    temperature: number
+    /** brightness in % */
+    brightness: number
+    /** fadeTime in seconds */
+    fadeTime: number
   }
 
 }
